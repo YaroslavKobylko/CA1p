a8f7f5a5f37f75a109908e33e2b711483cd48e7c
Yaroslav Kobylko <kobylkoyaroslav@gmail.com>
2024-01-15 16:52:37 +0200

    First commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,15 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import TopBar from './components/TopBar/TopBar'; // імпорт вашого компоненту
+import Header from './components/Header/Header'; // імпорт вашого компоненту
+import MainPage from './components/MainPage/MainPage'; // імпорт вашого компоненту
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <TopBar />
+      <Header />
+      <MainPage />
     </div>
   );
 }
